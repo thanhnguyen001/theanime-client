2e0efb2a2860c063dc40ee7e99a62f9edfa2875c
NguyenThanh <71008459+thanhnguyen001@users.noreply.github.com>
2021-09-29 15:24:44 +0700

    Done

--- src/components/Contents/Recommended/Recommended.js
+++ src/components/Contents/Recommended/Recommended.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import axiosClient from '../../../api/axiosClient';
 import CardAnime from '../../CardAnime/CardAnime';
@@ -7,7 +7,6 @@ import SkeletonCardAnime from '../../Skeleton/SkeletonCardAnime';
 function Recommended() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [updateList, setUpdateList] = useState([]);
-    const itemCurrent = useRef(0);
 
     useEffect(() => {
         const fetchUpdatingList = async () => {
@@ -35,24 +34,6 @@ function Recommended() {
         }
     }, [isLoaded])
 
-
-
-    const showItemFirstTime = (updateList) => {
-        let max = 0;
-        if (updateList.length > 0) {
-            let result = [];
-            max = updateList.length < 12 ? updateList.length : 11;
-            itemCurrent.current = max;
-            for (let i = 0; i <= max; i++) {
-                result.push(<div className="col lg-3 md-4 c-6 c-12 update-item ta-anime" key={i}>
-                    <CardAnime anime={updateList[i]} />
-                </div>)
-            }
-
-            return result;
-        }
-    }
-
     const showUpdatingList = (updateList) => {
         if (updateList.length > 0) {
             return updateList.map((item, index) => {
@@ -69,7 +50,7 @@ function Recommended() {
                 <Link to='/hom-nay-xem-gi'>Hôm nay xem gì</Link>
             </div>
             <div className="row">
-                {isLoaded ? showItemFirstTime(updateList) : [1, 2, 3, 4, 5, 6, 7, 8].map(n => {
+                {isLoaded ? showUpdatingList(updateList) : [1, 2, 3, 4, 5, 6, 7, 8].map(n => {
                     return <SkeletonCardAnime key={n} />
                 })}
             </div>
--- src/components/SignForm/SignForm.js
+++ src/components/SignForm/SignForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './SignForm.css';
 import { useForm } from 'react-hook-form';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
